ecb83d097637821dd4fe9dbc73c900a8d6d036fb
marijapejatovic <pejatovicmarija28@gmail.com>
2026-06-03 09:41:57 +0200

    Delete bronze directory

--- bronze/main.py
+++
@@ -1,9 +0,0 @@
-from load import load_bronze
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-engine = create_engine(os.getenv("DATABASE_URL"))
-df = load_bronze(engine)
-print(f"Bronze učitano: {len(df)} redova")
